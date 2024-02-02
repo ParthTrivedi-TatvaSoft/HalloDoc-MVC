ba939433a12a68ddba24ac659b1c24845c98d6a5
ParthTrivedi-TatvaSoft <parth.trivedi@etatvasoft.com>
2024-02-02 19:08:42 +0530

    Intial  Commit

---
+++ HalloDoc.mvc/Controllers/PatientController.cs
@@ -0,0 +1,32 @@
+﻿using HalloDoc.mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HalloDoc.mvc.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly ILogger<PatientController> _logger;
+
+        public PatientController(ILogger<PatientController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult submit_request()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HalloDoc.mvc/wwwroot/js/site.js
@@ -0,0 +1,40 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    var img = element.querySelector(".light")
+    img.src.includes("dark") ? img.src = "images/light.png" : img.src = "images/dark.png";
+}
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("myBtn");
+
+$(document).ready(function () {
+    modal.style.display = "block";
+});
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
